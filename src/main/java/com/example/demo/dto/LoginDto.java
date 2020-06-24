c806b5ff1b867cf1727003ec5cd934823dc618c6
supriya-nandana <nandanasupriya98@gmail.com>
2020-06-24 17:17:17 +0530

    base commit

---
+++ src/main/java/com/example/demo/dto/LoginDto.java
@@ -0,0 +1,29 @@
+/**
+
+ * 
+ */
+package com.example.demo.dto;
+/**
+ * @author 91970
+ *
+ */
+
+public class LoginDto {
+	String userName;
+	String password;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
+
+
